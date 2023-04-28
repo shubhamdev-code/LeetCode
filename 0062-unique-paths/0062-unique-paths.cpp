7ed5973d408b220788cea9fffaa768c71025077e
Shubham Kumar <73250851+shubhamdev-code@users.noreply.github.com>
2023-04-28 21:57:09 +0530

    Time: 3 ms (34.79%), Space: 6 MB (75.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> arr(n,1);
-        int prev_ans =1, curr_ans = 1; 
-        for(int i = 1; i<m; i++){
-            prev_ans = 1; 
-            for(int j = 1; j<n; j++){
-                curr_ans = prev_ans + arr[j];
-                arr[j] = curr_ans; 
-                prev_ans = curr_ans;  
-            }
+        int distance = m+n-2,rows = m-1; 
+        double ans = 1; 
+        
+        for(int i = 1; i<=rows; i++){
+            ans = ans * (distance-rows+i)/i; 
         }
-        return curr_ans; 
+        return (int)ans; 
     }
 };
