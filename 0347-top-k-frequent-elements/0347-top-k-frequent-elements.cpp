ec2dce145996a576d4a0a11607f78f621327365d
Shubham Kumar <73250851+shubhamdev-code@users.noreply.github.com>
2023-05-09 21:35:19 +0530

    Time: 32 ms (5.59%), Space: 15.6 MB (7.20%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> frq; 
+        for(int i =0; i<nums.size(); i++){
+            frq[nums[i]]++; 
+        }
+        
+        map<int,set<int>,greater<int>> sorted_by_val; 
+        for(auto i : frq){
+            sorted_by_val[i.second].insert(i.first);    
+        }
+        
+        int j =0 ; 
+        for(auto i : sorted_by_val){
+            for(auto k : i.second)
+                nums[j++] = k;
+        }
+
+        vector<int> ans; 
+        while(k--){
+            ans.push_back(nums[k]);
+        }
+        return ans;
+    }
+};
